3a7582ead6804d4b4905cd17cb8f876388ad9ee7
Arne Bosien <info@arnebosien.de>
2018-04-28 19:05:25 +0200

    somehow working

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+export * from './dist/appender';
+export * from './dist/console.appender';
+export * from './dist/logger';
+export * from './dist/log-level';
+export * from './dist/logging-backend';
